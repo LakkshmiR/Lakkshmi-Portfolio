cfa76a8cecf71d75f6135dc514676bf347c1c76b
LakkshmiR <rrlakkshmi@gmail.com>
2025-06-17 11:21:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function hamburg() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(0px)";
+}
+function cancel() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(-500px)";
+}
 document.getElementById("show-btn").addEventListener("click", function () {
-  window.open("C:Users/baska/Downloads/Lakkshmi R Resume.pdf", "_blank");
+  window.open("LakkshmiResume.pdf", "_blank");
 });
